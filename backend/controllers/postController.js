c786e97360e65d95ad18e602f70c6cc73d355fb9
RidloGhifary <ridloghfry@gmail.com>
2024-04-05 08:35:24 +0700

    update getallpost

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -4,7 +4,11 @@ const { validationResult } = require("express-validator");
 
 const GetAllPost = async (req, res) => {
   try {
-    const posts = await Post.find();
+    const posts = await Post.aggregate([
+      {
+        $sort: { numLikes: -1, createdAt: -1 },
+      },
+    ]);
     res.status(200).json(posts);
   } catch (err) {
     res.status(500).json({ message: "Internal server error" });
