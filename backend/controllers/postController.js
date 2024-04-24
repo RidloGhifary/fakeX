c9411f0b28c5750088de46e8e535d5bcec91001c
RidloGhifary <ridloghfry@gmail.com>
2024-04-24 13:56:22 +0700

    feat: change sorting getPostByUser

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -57,10 +57,12 @@ const GetPostByUserCreator = async (req, res) => {
     const user = await User.findOne({ username });
     if (!user) return res.status(404).json({ message: "User not found" });
 
-    const posts = await Post.find({ user: user._id }).populate({
-      path: "user",
-      select: "_id username bio profile_picture followers hasBadge",
-    });
+    const posts = await Post.find({ user: user._id })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "_id username bio profile_picture followers hasBadge",
+      });
     res.status(200).json(posts);
   } catch (err) {
     res.status(500).json({ message: "Internal server error" });
